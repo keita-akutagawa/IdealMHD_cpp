7d6a01ee8f70e5c104daa4c89e8e9074f2d20b03
Keita <keita19990517@gmail.com>
2024-05-17 10:18:37 +0900

    20240517

--- lib_IdealMHD_1D/unittests/test_muscl.cpp
+++ lib_IdealMHD_1D/unittests/test_muscl.cpp
@@ -22,5 +22,24 @@ TEST(MUSCLTest, CheckConstInputConstOutput)
     }
 }
 
+TEST(MUSCLTest, CheckDeltaInputChangeOutput)
+{
+    int n = 5;
+    std::vector<double> q = std::vector<double>(5, 0.0);
+    std::vector<double> qLeft = std::vector<double>(5, 0.0);
+    std::vector<double> qRight = std::vector<double>(5, 0.0);
+    q[2] = 1.0;
+
+    MUSCL muscl;
+
+    muscl.getLeftComponent(q, qLeft);
+    muscl.getRightComponent(q, qRight);
+
+    for (int i = 0; i < n; i++) {
+        EXPECT_NEAR(qLeft[i], q[i], 1e-10);
+        EXPECT_NEAR(qRight[i], q[i], 1e-10);
+    }
+}
+
 
 
