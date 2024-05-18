49218d895943eadad728b549af7b59c8b4330922
Keita <keita19990517@gmail.com>
2024-05-18 15:02:41 +0900

    20240518

--- lib_IdealMHD_1D/unittests/test_calculate_half_components.cpp
+++ lib_IdealMHD_1D/unittests/test_calculate_half_components.cpp
@@ -42,3 +42,70 @@ TEST(CalculateHalfComponentsTest, setPhysicalParameters)
     }
 }
 
+
+TEST(CalculateHalfComponentsTest, getLeftRightParameters)
+{
+    std::vector<std::vector<double>> U(8, std::vector<double>(nx, 1.0));
+
+    CalculateHalfComponents calculateHalfComponents;
+
+    calculateHalfComponents.setPhysicalParameters(U);
+    calculateHalfComponents.calculateLeftComponents();
+    calculateHalfComponents.calculateRightComponents();
+
+    Components componentsCenter, componentsLeft, componentsRight;
+    componentsCenter = calculateHalfComponents.getCenterComponents();
+    componentsLeft = calculateHalfComponents.getLeftComponents();
+    componentsRight = calculateHalfComponents.getRightComponents();
+
+    for (int i = 0; i < nx; i++) {
+        EXPECT_EQ(componentsCenter.rho[i], 1.0);
+        EXPECT_EQ(componentsLeft.rho[i], 1.0);
+        EXPECT_EQ(componentsRight.rho[i], 1.0);
+    }
+}
+
+
+TEST(CalculateHalfComponentsTest, getLeftRightParametersAgain)
+{
+    std::vector<std::vector<double>> U(8, std::vector<double>(nx, 1.0));
+
+    CalculateHalfComponents calculateHalfComponents;
+
+    calculateHalfComponents.setPhysicalParameters(U);
+    calculateHalfComponents.calculateLeftComponents();
+    calculateHalfComponents.calculateRightComponents();
+
+    Components componentsCenter, componentsLeft, componentsRight;
+    componentsCenter = calculateHalfComponents.getCenterComponents();
+    componentsLeft = calculateHalfComponents.getLeftComponents();
+    componentsRight = calculateHalfComponents.getRightComponents();
+
+    for (int i = 0; i < nx; i++) {
+        EXPECT_EQ(componentsCenter.rho[i], 1.0);
+        EXPECT_EQ(componentsLeft.rho[i], 1.0);
+        EXPECT_EQ(componentsRight.rho[i], 1.0);
+    }
+
+
+    for (int comp = 0; comp < 8; comp++) {
+        for (int i = 0; i < nx; i++) {
+            U[comp][i] = 2.0;
+        }
+    }
+
+    calculateHalfComponents.setPhysicalParameters(U);
+    calculateHalfComponents.calculateLeftComponents();
+    calculateHalfComponents.calculateRightComponents();
+
+    componentsCenter = calculateHalfComponents.getCenterComponents();
+    componentsLeft = calculateHalfComponents.getLeftComponents();
+    componentsRight = calculateHalfComponents.getRightComponents();
+
+    for (int i = 0; i < nx; i++) {
+        EXPECT_EQ(componentsCenter.rho[i], 2.0);
+        EXPECT_EQ(componentsLeft.rho[i], 2.0);
+        EXPECT_EQ(componentsRight.rho[i], 2.0);
+    }
+}
+
