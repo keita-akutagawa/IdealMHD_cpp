774c2282d95d2da6ba5811a53dbee91ffceedd5e
Keita <keita19990517@gmail.com>
2024-05-19 22:48:31 +0900

    20240519

--- lib_IdealMHD_1D/unittests/test_hlld.cpp
+++ lib_IdealMHD_1D/unittests/test_hlld.cpp
@@ -19,3 +19,64 @@ TEST(HLLDTest, Constructor)
     }
 }
 
+TEST(HLLDTest, CalculateFluxConstructor)
+{
+    std::vector<std::vector<double>> U(8, std::vector<double>(nx, 0.0));
+
+    HLLD hlld;
+    Flux flux;
+
+    hlld.calculateFlux(U);
+    flux = hlld.getFlux();
+
+    for (int comp = 0; comp < 8; comp++) {
+        for (int i = 0; i < nx; i++) {
+            EXPECT_EQ(flux.flux[comp][i], 0.0);
+        }
+    }
+}
+
+TEST(HLLDTest, CalculateFluxConstU)
+{
+    std::vector<std::vector<double>> U(8, std::vector<double>(nx, 1.0));
+
+
+    HLLD hlld;
+    Flux flux;
+
+    hlld.calculateFlux(U);
+    flux = hlld.getFlux();
+
+    for (int comp = 0; comp < 8; comp++) {
+        for (int i = 0; i < nx; i++) {
+            EXPECT_EQ(flux.flux[comp][i], 0.0);
+        }
+    }
+}
+
+TEST(HLLDTest, CalculateFluxNotConstU)
+{
+    std::vector<std::vector<double>> U(8, std::vector<double>(nx, 1.0));
+
+    for (int comp = 0; comp < 8; comp++) {
+        for (int i = 0; i < nx; i++) {
+            if ((i > 10) && (i < nx - 10)) {
+                U[comp][i] = 2.0;
+            }
+        }
+    }
+
+
+    HLLD hlld;
+    Flux flux;
+
+    hlld.calculateFlux(U);
+    flux = hlld.getFlux();
+
+    for (int comp = 0; comp < 8; comp++) {
+        for (int i = 0; i < nx; i++) {
+            EXPECT_NE(flux.flux[comp][i], 0.0);
+        }
+    }
+}
+
